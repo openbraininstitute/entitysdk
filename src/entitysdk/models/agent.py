23ea79c45b04e766a85537c4dd7f79265de3b2c3
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-02-28 13:49:21 +0100

    Add descriptions in fields

--- src/entitysdk/models/agent.py
+++ src/entitysdk/models/agent.py
@@ -1,18 +1,40 @@
 """Agent models."""
 
+from pydantic import Field
+
 from entitysdk.models.core import Struct
 
 
 class Person(Struct):
     """Person model."""
 
-    givenName: str
-    familyName: str
-    pref_label: str
+    givenName: str = Field(
+        ...,
+        examples=["John", "Jane"],
+        description="The given name of the person.",
+    )
+    familyName: str = Field(
+        ...,
+        examples=["Doe", "Smith"],
+        description="The family name of the person.",
+    )
+    pref_label: str = Field(
+        ...,
+        examples=["John Doe", "Jane Smith"],
+        description="The preferred label of the person.",
+    )
 
 
 class Organization(Struct):
     """Organization model."""
 
-    pref_label: str
-    alternative_name: str | None = None
+    pref_label: str = Field(
+        ...,
+        examples=["Open Brain Institute"],
+        description="The preferred label of the organization.",
+    )
+    alternative_name: str | None = Field(
+        default=None,
+        examples=["Open Brain Institute"],
+        description="The alternative name of the organization.",
+    )
--- src/entitysdk/models/base.py
+++ src/entitysdk/models/base.py
@@ -2,9 +2,10 @@
 
 from datetime import datetime
 from typing import Self
+from uuid import UUID
 
 from pydantic import BaseModel as PydanticBaseModel
-from pydantic import ConfigDict
+from pydantic import ConfigDict, Field
 
 
 class BaseModel(PydanticBaseModel):
@@ -15,12 +16,26 @@ class BaseModel(PydanticBaseModel):
         from_attributes=True,
         extra="forbid",
     )
-
-    update_date: datetime | None = None
-    creation_date: datetime | None = None
-
-    authorized_public: bool | None = None
-    authorized_project_id: str | None = None
+    update_date: datetime | None = Field(
+        default=None,
+        examples=[datetime(2025, 1, 1)],
+        description="The date and time the resource was last updated.",
+    )
+    creation_date: datetime | None = Field(
+        default=None,
+        examples=[datetime(2025, 1, 1)],
+        description="The date and time the resource was created.",
+    )
+    authorized_public: bool | None = Field(
+        default=None,
+        examples=[True, False],
+        description="Whether the resource is authorized to be public.",
+    )
+    authorized_project_id: UUID | None = Field(
+        default=None,
+        examples=[UUID("12345678-1234-1234-1234-123456789012")],
+        description="The project ID the resource is authorized to be public.",
+    )
 
     def evolve(self, **model_attributes) -> Self:
         """Evolve a copy of the model with new attributes."""
