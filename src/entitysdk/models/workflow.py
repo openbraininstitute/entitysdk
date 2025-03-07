52b6885455375b9c73bfe51f9bc1bed1cf8cbd80
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-03-07 08:34:38 +0100

    Import datetime

--- src/entitysdk/models/workflow.py
+++ src/entitysdk/models/workflow.py
@@ -1,9 +1,10 @@
+from datetime import datetime
+
 from entitysdk.mixin import HasAssets
 from entitysdk.models.core import Activity
 
 
 class WorkflowExecution(HasAssets, Activity):
-
     module: str
     task: str
     configFileName: str
