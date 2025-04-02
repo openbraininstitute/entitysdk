43f2e3d61441ce595b700da477bcc175783ec198
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2025-04-02 09:34:38 +0200

    Update src/entitysdk/common.py
    Co-authored-by: Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>

--- src/entitysdk/common.py
+++ src/entitysdk/common.py
@@ -13,7 +13,7 @@ UUID_RE = "[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}"
 
 
 VLAB_URL_PATTERN = re.compile(
-    r"^https:\/\/(?P<env>staging|www)?\.openbraininstitute\.org"
+    r"^https:\/\/(?P<env>staging|www)\.openbraininstitute\.org"
     rf"\/app\/virtual-lab\/lab\/(?P<vlab>{UUID_RE})"
     rf"\/project\/(?P<proj>{UUID_RE})(?:\/.*)?$"
 )
