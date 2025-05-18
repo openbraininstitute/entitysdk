d3e9f3795d86762a414bc3e754bcd58512ffd246
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-05-18 15:59:21 +0200

    Refactor ReconstructionMorphology

--- src/entitysdk/models/morphology.py
+++ src/entitysdk/models/morphology.py
@@ -4,26 +4,17 @@ from typing import Annotated
 
 from pydantic import Field
 
-from entitysdk.mixin import HasAssets
 from entitysdk.models.brain_location import BrainLocation
 from entitysdk.models.brain_region import BrainRegion
-from entitysdk.models.contribution import Contribution
 from entitysdk.models.entity import Entity
 from entitysdk.models.license import License
 from entitysdk.models.mtype import MTypeClass
 from entitysdk.models.taxonomy import Species, Strain
 
 
-class ReconstructionMorphology(HasAssets, Entity):
+class ReconstructionMorphology(Entity):
     """Morphology model."""
 
-    name: Annotated[
-        str,
-        Field(
-            examples=["layer 5 Pyramidal Cell"],
-            description="The name of the morphology.",
-        ),
-    ]
     location: Annotated[
         BrainLocation | None,
         Field(
@@ -34,13 +25,6 @@ class ReconstructionMorphology(HasAssets, Entity):
         BrainRegion,
         Field(
             description="The region of the brain where the morphology is located.",
-        ),
-    ]
-    description: Annotated[
-        str,
-        Field(
-            examples=["A layer 5 pyramidal cell"],
-            description="The description of the morphology.",
         ),
     ]
     species: Annotated[
@@ -59,12 +43,6 @@ class ReconstructionMorphology(HasAssets, Entity):
         License | None,
         Field(
             description="The license attached to the morphology.",
-        ),
-    ] = None
-    contributions: Annotated[
-        list[Contribution] | None,
-        Field(
-            description="List of contributions.",
         ),
     ] = None
     mtypes: Annotated[
