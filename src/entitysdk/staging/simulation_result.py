9966f1ffd9d8dabb39d1fee35125c164d4c1c70e
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-06-20 21:15:45 +0200

    Add tests for staging SimulationResult and Simulation

--- src/entitysdk/staging/simulation_result.py
+++ src/entitysdk/staging/simulation_result.py
@@ -8,11 +8,18 @@ from entitysdk.downloaders.simulation_result import (
     download_spike_report_file,
     download_voltage_report_files,
 )
-from entitysdk.models import SimulationResult
+from entitysdk.models import Simulation, SimulationResult
+from entitysdk.staging.simulation import stage_simulation
 from entitysdk.types import StrOrPath
 from entitysdk.utils.filesystem import create_dir
+from entitysdk.utils.io import load_json
 
 L = logging.getLogger(__name__)
+
+
+DEFAULT_REPORTS_DIR_NAME = "output"
+DEFAULT_SPIKE_FILE_NAME = "output/spikes.h5"
+DEFAULT_SIMULATION_CONFIG_FILENAME = "simulation_config.json"
 
 
 def stage_simulation_result(
@@ -20,16 +27,39 @@ def stage_simulation_result(
     *,
     model: SimulationResult,
     output_dir: StrOrPath,
-):
+    simulation_config_file: StrOrPath | None = None,
+) -> Path:
     """Stage a SimulationResult entity."""
     output_dir: Path = create_dir(output_dir)
+
+    if simulation_config_file is None:
+        simulation_config_file = stage_simulation(
+            client,
+            model=client.get_entity(entity_id=model.simulation_id, entity_type=Simulation),
+            output_dir=output_dir,
+        )
+    else:
+        assert Path(simulation_config_file).parent == output_dir
+
+    config: dict = load_json(simulation_config_file)
+    reports_dir, spikes_file = _get_output_paths(config, output_dir)
+    create_dir(reports_dir)
+
     download_spike_report_file(
         client,
         model=model,
-        output_path=output_dir / "spikes.h5",
+        output_path=spikes_file,
     )
     download_voltage_report_files(
         client,
         model=model,
-        output_dir=output_dir,
+        output_dir=reports_dir,
     )
+
+    return Path(simulation_config_file)
+
+
+def _get_output_paths(config: dict, output_dir: Path) -> tuple[Path, Path]:
+    reports_dir = output_dir / config["output"]["output_dir"]
+    spikes_file = output_dir / config["output"]["spikes_file"]
+    return reports_dir, spikes_file
