60baa7ca4d692a87f72083cf4676f14dfa759e0a
Gianluca Ficarelli <gianluca.ficarelli@gmail.com>
2025-08-19 15:24:56 +0200

    Add Asset.storage_type when retrieving assets from entitycore

--- tests/unit/downloaders/test_memodel.py
+++ tests/unit/downloaders/test_memodel.py
@@ -21,6 +21,7 @@ def _mock_morph_asset_response(asset_id):
         "status": "created",
         "meta": {},
         "sha256_digest": "sha256_digest",
+        "storage_type": "aws_s3_internal",
     }
 
 
@@ -37,6 +38,7 @@ def _mock_ic_asset_response(asset_id):
         "status": "created",
         "meta": {},
         "sha256_digest": "sha256_digest",
+        "storage_type": "aws_s3_internal",
     }
 
 
@@ -53,6 +55,7 @@ def _mock_emodel_asset_response(asset_id):
         "status": "created",
         "meta": {},
         "sha256_digest": "sha256_digest",
+        "storage_type": "aws_s3_internal",
     }
 
 
