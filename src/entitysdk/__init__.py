fe4caffc790cdab350b8950feaa4b2b5dd6754a1
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-11-07 14:54:15 +0100

    Expose DataMount

--- src/entitysdk/__init__.py
+++ src/entitysdk/__init__.py
@@ -2,5 +2,6 @@
 
 from entitysdk.client import Client
 from entitysdk.common import ProjectContext
+from entitysdk.mount import DataMount
 
-__all__ = ["Client", "ProjectContext"]
+__all__ = ["Client", "ProjectContext", "DataMount"]
