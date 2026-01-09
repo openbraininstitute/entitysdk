83e3e8f7f3f186ca27af68ef61dfd6c664a74247
James Isbister <isbisterjb@gmail.com>
2026-01-09 11:25:45 +0100

    add note

--- src/entitysdk/models/simulation.py
+++ src/entitysdk/models/simulation.py
@@ -10,4 +10,4 @@ class Simulation(Entity):
     simulation_campaign_id: ID
     entity_id: ID
     scan_parameters: dict
-    number_neurons: int | None = None
+    number_neurons: int | None = None # Make non optional after 2026.1.3 prod deployment
