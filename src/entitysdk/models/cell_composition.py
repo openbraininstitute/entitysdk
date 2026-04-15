ed0d16e44d31e7eaeb51bada6a2aa344cdd4b4cf
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-04-15 11:34:44 +0200

    Update CellComposition species/strain/brain_region

--- src/entitysdk/models/cell_composition.py
+++ src/entitysdk/models/cell_composition.py
@@ -1,7 +1,23 @@
 """Cell Composition."""
 
+from typing import Annotated
+
+from pydantic import Field
+
+from entitysdk.models.brain_region import BrainRegion
 from entitysdk.models.entity import Entity
+from entitysdk.models.taxonomy import Species, Strain
 
 
 class CellComposition(Entity):
     """Cell Composition."""
+
+    species: Annotated[
+        Species,
+        Field(description="The species for which the emodel applies."),
+    ]
+    strain: Annotated[
+        Strain | None,
+        Field(description="The specific strain of the species, if applicable."),
+    ] = None
+    brain_region: BrainRegion | None = None
