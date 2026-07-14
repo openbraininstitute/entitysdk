8de2dbb97d7cc80f85be314cb9b4820f0a42af16
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-07-14 17:07:33 +0200

    Add registration

---
+++ src/entitysdk/registration/emodel.py
@@ -0,0 +1,111 @@
+"""Module for registering emodels."""
+
+import logging
+from pathlib import Path
+
+from entitysdk.models import (
+    BrainRegion,
+    CellMorphology,
+    Derivation,
+    EModel,
+    Entity,
+    IonChannelModel,
+    License,
+    MTypeClass,
+    Species,
+)
+from entitysdk.registartion.validation_result import register_validation_result_figure
+from entitysdk.registration.classification import (
+    register_etype_classification,
+)
+from entitysdk.types import (
+    ID,
+    AssetLabel,
+    ContentType,
+    DerivationType,
+    EntityLifecycleStatus,
+    EntityType,
+)
+
+L = logging.getLogger(__name__)
+
+
+def register_emodel(
+    *,
+    client,
+    name: str,
+    description: str,
+    authorized_public: bool,
+    species: Species,
+    brain_region: BrainRegion,
+    license: License,  # noqa: A002
+    seed: int,
+    iteration: int,
+    score: float,
+    exemplar_morphology: CellMorphology,
+    ion_channel_models: list[IonChannelModel],
+    lifecycle_status: EntityLifecycleStatus,
+    etype_class: MTypeClass,
+    hoc_file: Path,
+    emodel_summary_file: Path,
+    electrical_cell_recording_ids: list[ID],
+    validation_result_figure_files: list[Path],
+    validateion_result_status: bool,
+):
+    """Register EModel."""
+    emodel = client.register_entity(
+        EModel(
+            name=name,
+            description=description,
+            authorized_public=authorized_public,
+            license=license,
+            seed=seed,
+            iteration=iteration,
+            score=score,
+            exemplar_morphology=exemplar_morphology,
+            lifecycle_status=lifecycle_status,
+        )
+    )
+    L.info("Registered EModel(id=%s, name=%s)", emodel.id, name)
+    register_etype_classification(
+        client=client,
+        entity=emodel,
+        etype_class=etype_class,
+    )
+    asset = client.upload_file(
+        entity_id=emodel.id,
+        entity_type=EModel,
+        file_path=hoc_file,
+        file_content_type=ContentType.application_hoc,
+        asset_label=AssetLabel.neuron_hoc,
+    )
+    L.info("Registered EModel Asset(id=%s, label=%s, path=%s)", asset.id, asset.label, asset.path)
+    asset = client.upload_file(
+        entity_id=emodel.id,
+        entity_type=EModel,
+        file_path=emodel_summary_file,
+        file_content_type=ContentType.application_json,
+        asset_label=AssetLabel.emodel_optimization_output,
+    )
+    L.info("Registered EModel Asset(id=%s, label=%s, path=%s)", asset.id, asset.label, asset.path)
+    for trace_id in electrical_cell_recording_ids:
+        derivation = client.register_entity(
+            Derivation(
+                used=Entity(
+                    id=trace_id,
+                    type=EntityType.electrical_cell_recording,
+                ),
+                generated=emodel,
+                derivation_type=DerivationType.unspecified,
+            )
+        )
+        L.info("Registered EModel Derivation(id=%s, used=%s)", derivation.id, trace_id)
+
+    for figure_file in validation_result_figure_files:
+        register_validation_result_figure(
+            client=client,
+            authorized_public=emodel.authorized_public,
+            figure_file=figure_file,
+            passed=False,
+        )
+    return emodel
